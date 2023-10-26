274df3b41a27dc592411cd05e6f736bd6a19a681
Erfan Baghalpour <erfanbaghalpour@gmail.com>
2023-10-26 22:22:20 +0330

    Pagination in drf

--- ads/views.py
+++ ads/views.py
@@ -4,11 +4,12 @@ from rest_framework.views import APIView
 from .models import Ad
 from .serializers import AdSerializer
 from rest_framework import status
+from .pagination import StandardResultsSetPagination
 
-
-class AdListView(APIView):
+class AdListView(APIView, StandardResultsSetPagination):
     serializer_class = AdSerializer
     def get(self, request):
         queryset = Ad.objects.filter(is_public=True)
-        serializer = AdSerializer(instance=queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        result = self.paginate_queryset(queryset, request)
+        serializer = AdSerializer(instance=result, many=True)
+        return self.get_paginated_response(serializer.data)
