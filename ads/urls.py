c732ddf269ce571c5b639d0da0b06efc044ac2cf
Erfan Baghalpour <erfanbaghalpour@gmail.com>
2023-10-30 20:29:45 +0330

    Ad Detail

--- ads/urls.py
+++ ads/urls.py
@@ -5,4 +5,5 @@ app_name = 'ads'
 urlpatterns = [
     path('list', views.AdListView.as_view(), name='ad_list'),
     path('create', views.AdCreateView.as_view(), name='ad_create'),
+    path('detail/<int:pk>', views.AdDetailView.as_view(), name='ad_detail'),
 ]
